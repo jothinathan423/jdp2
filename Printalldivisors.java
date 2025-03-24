8b44f2de44657b168804d595d6d3f14665b8c61c
jothinathan423 <jothinathannagarajan@gmail.com>
2025-03-24 19:58:04 +0530

    jothinathan

---
+++ Printalldivisors.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Printalldivisors {
+    public static void main(String args[]) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i != 0) {
+                System.out.print(i + "\t");
+            }
+        }
+    }
+}
